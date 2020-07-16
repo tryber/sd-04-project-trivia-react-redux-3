d8e5e9b16deea2f0ac57ff6e2f2d87c017b5eb6c
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-16 17:47:51 -0300

    add button configuratiob

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logInto } from '../actions/login';
-import { Link } from 'react-router-dom';
 
 const renderLabelInput = (textLabel, type, id, name, dataTestid, user, setUser) => (
   <label htmlFor={id}>
