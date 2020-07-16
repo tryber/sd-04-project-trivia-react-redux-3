73ea6caf4b5dd1b33368be28b3ee50d7f0b3b7f6
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-16 16:20:50 -0300

    add login codeclimate

--- src/services/triviaAPI.js
+++
@@ -1,8 +0,0 @@
-const getToken = () =>
-  fetch('https://opentdb.com/api_token.php?command=request').then((token) =>
-    token
-      .json()
-      .then((json) => (token.ok ? Promise.resolve(json) : Promise.reject(json))),
-  ),
-
-  export default getToken;
