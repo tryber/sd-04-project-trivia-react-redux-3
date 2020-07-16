82dbc3dbbbc883d46ff196865021b4a7ea4f9c29
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-16 16:18:10 -0300

    add login codeclimate

--- src/services/triviaAPI.js
+++ src/services/triviaAPI.js
@@ -1,24 +1,8 @@
-export const getToken = () =>
+const getToken = () =>
   fetch('https://opentdb.com/api_token.php?command=request').then((token) =>
     token
       .json()
       .then((json) => (token.ok ? Promise.resolve(json) : Promise.reject(json))),
   ),
 
-  export const getCategory = () =>
-  fetch('https://opentdb.com/api_category.php').then((category) =>
-    category
-      .json()
-      .then((json) =>category.ok ? Promise.resolve(json) : Promise.reject(json)),
-  ),
-  
-export const getTrivia = (token, category, difficulty, type) =>
-  fetch(
-    `https://opentdb.com/api.php?amount=5&token=${token}&category=${category}&difficulty=${difficulty}&type=${type}`
-  ).then((trivia) =>
-    trivia
-      .json()
-      .then((json) =>
-        trivia.ok ? Promise.resolve(json) : Promise.reject(json),
-      ),
-  ),
+  export default getToken;
