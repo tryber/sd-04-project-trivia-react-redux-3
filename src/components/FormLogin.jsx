4a9faa48753130706f86aced4b197071f728e965
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-16 16:07:51 -0300

    add login codeclimate

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { logInto } from '../actions/login';
 
+const renderLabelInput = (textLabel, type, id, name, dataTestid, user, setUser) => (
+  <label htmlFor={id}>
+    {textLabel}
+    <input
+      id={id}
+      name={name}
+      type={type}
+      value={user[name]}
+      data-testid={dataTestid}
+      onChange={(event) =>
+        setUser({ ...user, [event.target.name]: event.target.value })
+      }
+    />
+  </label>
+);
+
 const FormLogin = () => {
   const [user, setUser] = useState({
     name: '',
@@ -9,28 +25,11 @@ const FormLogin = () => {
   });
   const dispatch = useDispatch();
 
-  const renderLabelInput = (textLabel, type, id, name, dataTestid) => (
-    <label htmlFor={id}>
-      {textLabel}
-      <input
-        id={id}
-        name={name}
-        type={type}
-        value={user[name]}
-        data-testid={dataTestid}
-        onChange={(event) =>
-          setUser({ ...user, [event.target.name]: event.target.value })
-        }
-      />
-    </label>
-  );
-  
-
   return (
     <div>
       <form>
-        {renderLabelInput('insira o email', 'email', 'email', 'emailGravatar', 'input-gravatar-email')}
-        {renderLabelInput('insira o nome', 'text', 'name', 'name', 'input-player-name')}
+        {renderLabelInput('insira o email', 'email', 'email', 'emailGravatar', 'input-gravatar-email', user, setUser)}
+        {renderLabelInput('insira o nome', 'text', 'name', 'name', 'input-player-name', user, setUser)}
         <input
           value="jogar"
           id="jogar"
--- src/services/triviaAPI.js
+++ src/services/triviaAPI.js
@@ -2,8 +2,8 @@ export const getToken = () =>
   fetch('https://opentdb.com/api_token.php?command=request').then((token) =>
     token
       .json()
-      .then((json) => (token.ok ? Promise.resolve(json) : Promise.reject(json)));
-  );
+      .then((json) => (token.ok ? Promise.resolve(json) : Promise.reject(json))),
+  ),
 
 export const getTrivia = (token, category, difficulty, type) =>
   fetch(
