c9c5bf204159cc306935839a6ade580ed26f01c9
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-17 19:01:39 -0300

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import token from './token';
 import login from './login';
-
+import trivia from './trivia';
 const rootReducer = combineReducers({
   login,
   token,
+  trivia,
 });
 
 export default rootReducer;
