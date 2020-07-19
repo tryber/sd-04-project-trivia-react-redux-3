a4cc9eb30622d2ac24662232d5018a0a3b14621b
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-07-19 16:03:57 -0300

    req 08 implementaçao pontuaçao e next button done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import token from './token';
 import login from './login';
-import trivia from './trivia';
+import ButtonAnswer from './ButtonAnswer';
 
 const rootReducer = combineReducers({
   login,
   token,
-  trivia,
+  ButtonAnswer,
 });
 
 export default rootReducer;
