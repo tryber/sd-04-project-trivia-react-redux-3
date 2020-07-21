28df8f0510da4d99b0e294aea0345c9157e9dcf1
igorgbr <igorgphotoarte@gmail.com>
2020-07-21 03:56:48 -0300

    conflitos resolvidos cc 3

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Ranking() {
+const Ranking = () => {
   const players = JSON.parse(localStorage.getItem('ranking'));
   players.sort((a, b) => b.score - a.score);
-  const renderPlayers = () => players.map((element, index) => (
-    <li key={`${element.name}-${index}`}>
-      <img src={element.picture} alt="imagem do gravatar" />
-      <span data-testid={`player-name-${index}`}>{element.name}</span>
-      <span data-testid={`player-score-${index}`}> | {element.score}</span>
-    </li>
-  ));
+  const player = () =>
+    players.map((element, index) =>
+        <li key={`${element.name}-${index}`}>
+          <img src={element.picture} alt="foto-perfil" />
+          <p data-testid={`player-name-${index}`}>Jogador: {element.name}</p>
+          <p data-testid={`player-score-${index}`}>Pontos: {element.score}</p>
+        </li>
+    );
+
   return (
     <div>
       <h2 data-testid="ranking-title">Ranking</h2>
-      <ol>{renderPlayers()}</ol>
-      <Link to="/">
-        <button data-testid="btn-go-home">Home</button>
-      </Link>
+      <ol>{player()}</ol>
+      <Link data-testid="btn-go-home" to="/">Go home</Link>
     </div>
   );
-}
+};
+
+export default Ranking;
