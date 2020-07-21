fe3612cb0f2de6f50ffce310fd42b9d08eb20fa5
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-07-21 09:47:21 -0300

    merge with tela_configuration remove butonanswer reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 import token from './token';
 import login from './login';
 import trivia from './trivia';
-import ButtonAnswer from './ButtonAnswer';
 import StoreConfiguration from '../reducers/StoreConfiguration'
 
 const rootReducer = combineReducers({
   login,
   token,
   trivia,
-  ButtonAnswer,
   StoreConfiguration,
 });
 
