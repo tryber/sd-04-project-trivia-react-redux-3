90930f69c490c7e7b17db4bbaa53ddf5cc99a6fb
igorgbr <igorgphotoarte@gmail.com>
2020-07-21 03:20:59 -0300

    teste

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,9 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function Ranking() {
   const players = JSON.parse(localStorage.getItem('ranking'));
-  console.log('players', players);
+  players.sort((a, b) => b.score - a.score);
   return (
     <div>
+      <h2 data-testid="ranking-title">Ranking</h2>
+      <ol>
+        {players.map((element, index) => (
+          <li key={`${element.name}-${index}`}>
+            <img src={element.picture} />
+            <span data-testid={`player-name-${index}`}>{element.name}</span>
+            <span data-testid={`player-score-${index}`}> | {element.score}</span>
+          </li>
+        ))}
+      </ol>
       <Link to="/">
         <button data-testid="btn-go-home"> Home</button>
       </Link>
